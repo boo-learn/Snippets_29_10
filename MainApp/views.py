1b1cb905e5a4b33fcae8a7bf71d5687e7e76f0d5
boo <boo>
2021-08-06 07:47:54 -0700

    fix gitignore and templates

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,22 +2,16 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 
 
-def get_base_context(request, pagename):
-    return {
-        'pagename': pagename
-    }
-
-
 def index_page(request):
-    context = get_base_context(request, 'PythonBin')
+    context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
 
 def add_snippet_page(request):
-    context = get_base_context(request, 'Добавление нового сниппета')
+    context = {'pagename': 'Добавление нового сниппета'}
     return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
-    context = get_base_context(request, 'Просмотр сниппетов')
+    context = {'pagename': 'Просмотр сниппетов'}
     return render(request, 'pages/view_snippets.html', context)
