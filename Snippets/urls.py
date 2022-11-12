bd5824a99f6f0fb7a46e332058e2ca46a3794bd1
boo <booblegum42@gmail.com>
2022-11-12 16:03:00 +0700

    step-3 complete

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('', views.index_page, name='home'),
+    path('snippets/add', views.add_snippet_page, name='snippet-add'),
+    path('snippets/list', views.snippets_page, name='snippet-list'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
