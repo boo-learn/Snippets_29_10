c5f0b7e2b00f8a3cd4564c72bd0eb4d3c3d47760
boo <booblegum42@gmail.com>
2022-11-26 18:28:15 +0700

    fix ALLOWED_HOSTS

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#%3rmpy6hb52y7j^tu7g*v$762rc0&f+26yy3x!j&+@s4(ndq@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
