b5b6a93c2520423b789b7f51d5c8dbe259da9b03
boo <booblegum42@gmail.com>
2022-11-26 18:52:36 +0700

    admin on

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from MainApp.models import Snippet
 
-# Register your models here.
+admin.site.register(Snippet)
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippet-add'),
     path('snippets/list', views.snippets_page, name='snippet-list'),
